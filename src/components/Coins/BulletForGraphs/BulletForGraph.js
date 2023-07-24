383740a3c49c13ae992eafe063b6bc4933b77e09
ONYEDIKACHUKWU CHIEDU <77061016+chieduchineme@users.noreply.github.com>
2023-07-24 16:54:32 +0100

    Update BulletForGraph.js

--- src/components/Coins/BulletForGraphs/BulletForGraph.js
+++ src/components/Coins/BulletForGraphs/BulletForGraph.js
@@ -5,10 +5,10 @@ const BulletForGraphs = (props) => {
     return (
       <>
         {props.coinInfo !== null && (
-          <ul className={styles.ul}>
-            <li className={styles.red_bullet}>1 {props.symbol}= $ {props.price}</li>
+          <ul style={{position: "relative"}}>
+            <li style={{marginBottom:"-6em", marginLeft:"6em", fontWeight:"bold" }} className={styles.red_bullet}>1 {props.symbol}= $ {props.price}</li>
 
-          <div className={styles.bullet_list}>
+          <div style={{display:"flex", flexDirection:"row", marginTop:"-2em",marginBottom:"-10em", marginLeft:"6em" }} className={styles.bullet_list}>
             <li  style={{ marginRight:"4em"  }} className={styles.green_bullet}>Lower: {props.highestPriceValue}</li>
             <li className={styles.orange_bullet}>Higher: {props.lowestPriceValue}</li>
           </div>
